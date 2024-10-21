d474fb807a033ba1bbdfc448c4c047d4fcd09bbf
4hoe8pow <124800114+4hoe8pow@users.noreply.github.com>
2024-10-21 07:44:14 +0000

    ⚡️

--- app/components/utils/formatDate.ts
+++ app/components/utils/formatDate.ts
@@ -1,14 +1,11 @@
 export const formatDate = (dateString: string) => {
 	const date = new Date(dateString)
 	const options = {
+		weekday: 'long',
 		year: 'numeric',
-		month: 'short',
+		month: 'long',
 		day: 'numeric',
 		timeZone: 'Asia/Tokyo',
 	} as const
-	const formatted = date.toLocaleDateString('en-US', options)
-	const day = date.getDate()
-	const daySuffix = (n: number) =>
-		n > 3 && n < 21 ? 'th' : ['st', 'nd', 'rd', 'th'][Math.min(n % 10, 3)]
-	return formatted.replace(/\d+/, `${day}${daySuffix(day)}`)
+	return date.toLocaleDateString('en-US', options)
 }
